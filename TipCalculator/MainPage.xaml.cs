120acd7539ea063c1b79598fa5b237390cf5f611
Graham Saulnier <grahamsaulnier@gmail.com>
2012-07-01 11:39:50 -0400

    adding comments

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -14,12 +14,12 @@ using Microsoft.Phone.Controls;
 namespace TipCalculator {
    public partial class MainPage : PhoneApplicationPage {
       // Constants
-      const String BILL= "Bill : $";
-      const String TIP= "Tip : $";
-      const String TIPPP= "Tip per person : $";
+      const String BILL = "Bill : $";
+      const String TIP = "Tip : $";
+      const String TIPPP = "Tip per person : $";
       const String BILLPP = "Bill per person : $";
       // Constructor
-      public MainPage () {
+      public MainPage() {
          InitializeComponent();
          totalbillblock.Visibility = Visibility.Collapsed;
          totaltipblock.Visibility = Visibility.Collapsed;
@@ -41,26 +41,53 @@ namespace TipCalculator {
       /// <param name="bill"></param>
       /// <param name="percent"></param>
       /// <returns></returns>
-      private double GetTip ( double bill , double percent) {
+      private double GetTip(double bill, double percent) {
          return bill * percent;
       }
       /// <summary>
-      /// Pass percent and tax as decimal
+      /// Gets the tip for the bill (calculates before tax)
       /// </summary>
       /// <param name="bill"></param>
       /// <param name="tax"></param>
       /// <param name="percent"></param>
       /// <returns></returns>
-      private double GetTipBeforeTax ( double bill, double tax, double percent ) {
+      private double GetTipBeforeTax(double bill, double tax, double percent) {
          return (GetTip(bill / (1 + tax), percent));
       }
-      private double GetTipPerPerson ( double tip, int people ) {
+      /// <summary>
+      /// Gets the tip amount per person
+      /// </summary>
+      /// <param name="tip"></param>
+      /// <param name="people"></param>
+      /// <returns></returns>
+      private double GetTipPerPerson(double tip, int people) {
          return tip / people;
       }
-      private double GetBillPerPerson ( double bill, double tip, int people ) {
+      /// <summary>
+      /// Gets the tip amount as a % of the bill
+      /// </summary>
+      /// <param name="total"></param>
+      /// <param name="tip"></param>
+      /// <returns></returns>
+      private double GetTipPercent(double total, double tip) {
+         return tip / total;
+      }
+      /// <summary>
+      /// This calculates the total amount each person will pay
+      /// </summary>
+      /// <param name="bill"></param>
+      /// <param name="tip"></param>
+      /// <param name="people"></param>
+      /// <returns></returns>
+      private double GetBillPerPerson(double bill, double tip, int people) {
          return ((bill + tip) / people);
       }
-      private void calculatebtn_Click ( object sender, RoutedEventArgs e ) {
+      /// <summary>
+      /// This method is called when you press the calculate button
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private void calculatebtn_Click(object sender, RoutedEventArgs e) {
          if ( billbox.Text == "" ) billbox.Text = "0";
          if ( taxbox.Text == "" ) taxbox.Text = "0";
          if ( tipbox.Text == "" ) tipbox.Text = "0";
